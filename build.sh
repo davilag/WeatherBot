e238f0e094b692bd5fc80d1855e93c894f939c43
David Avila <dag@bluetel.co.uk>
2017-01-05 22:53:22 +0000

    Changing in the check pip for pip3, also in the message

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Checking if pip is installed.
-hash pip 2>/dev/null || { echo >&2 "I require pip but it's not installed.  Aborting."; exit 1; }
+hash pip3 2>/dev/null || { echo >&2 "I require pip3 but it's not installed.  Aborting."; exit 1; }
 
 echo "Installing all dependencies..."
 
