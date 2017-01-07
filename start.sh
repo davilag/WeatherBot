a0ab09e4d74d047145c066e1c314c4d63d9d600a
David Avila <d.avilag23@gmail.com>
2017-01-07 20:18:28 +0000

    :(

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export  WEATHER_API_URL='https://api.darksky.net/forecast/'
-export  WEATHER_API_TOKEN='504d73a4c9b0f063c69f605af57f24d4'
-export  TELEGRAM_API_TOKEN='321799685:AAEt8DbIxzKuUFvkE9BhUpiXyQewiETXDm8'
+export  WEATHER_API_TOKEN=''
+export  TELEGRAM_API_TOKEN=''
 
 python3 start.py
